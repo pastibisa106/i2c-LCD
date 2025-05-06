f032a2a9cab8a45d94104021ea698d4f00a8e1a0
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-07 06:57:05 +0700

    Add files via upload

---
+++ I2C Arduino Code.ino
@@ -0,0 +1,25 @@
+/*
+  I2C ARDUINO
+  RAFIQUL HAKIM
+  065117002
+  24 SEPTEMBER 2018
+  17:55 WIB
+ */
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd (0x27 , 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+// Ubah alamat 0x27 dengan alamat i2c kamu
+ 
+ void setup() {
+  // put your setup code here, to run once:
+  lcd.begin (16, 2); //LCD untuk ukuran 16x2
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(2, 0); //baris pertama
+  lcd.print("TEST LCD i2C");
+  lcd.setCursor(4, 1); //baris kedua
+  lcd.print("BY VCOOL");
+}
